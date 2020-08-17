521dedcda0962d3698945c5b51becda2389cf96d
Austin Phy <atphy42@gmail.com>
2020-08-17 14:26:44 -0500

    added student isAlive functions and shapes

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -3,23 +3,27 @@ import React from 'react';
 import './MainPage.scss';
 
 import LiveStudent from './LiveStudent/LiveStudent';
-import Sharktank from './Sharktank/Sharktank';
-import GraveStone from './GraveStone/GraveStone';
+// import Sharktank from './Sharktank/Sharktank';
+// import GraveStone from './GraveStone/GraveStone';
 import Graveyard from './Graveyard/Graveyard';
 
 class MainPage extends React.Component {
   render() {
     return (
+
     <div className="main-page">
-      <div className="shark-attack-button" alt="shark attack button">
-        <img className="dead-shark" src="https://i.imgur.com/P1sGlcs.png" />
-        <img className="alive-ship" src="https://i.gifer.com/JHQ.gif" />
+
+      <div className="shark-attack-button">
+        <img className="dead-shark" src="https://i.imgur.com/P1sGlcs.png" alt="shark icon" />
+        <img className="alive-ship" src="https://i.gifer.com/JHQ.gif" alt="ship icon" />
       </div>
+
       <div className="students-container">
-      <LiveStudent className="live-student" />
-      <Graveyard className="graveyard" />
+        <LiveStudent className="live-student" />
+        <Graveyard className="graveyard" />
       <h2>hello</h2>
       </div>
+
     </div>
     );
   }
